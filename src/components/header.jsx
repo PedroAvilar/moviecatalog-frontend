6562c0bfbd8209ea8acd9814a9d62123894d2481
Pedro Wilson Araújo Avilar <pedrowa.avilar@gmail.com>
2026-01-09 17:55:36 -0300

    refactor: it simplifies the menu and tests the active item

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,14 +2,30 @@ import { NavLink } from 'react-router-dom';
 import './header.css';
 
 function Header() {
+    // Define os itens do menu de navegação
+    const menuItems = [
+        { label: 'Página inicial', path: '/', end: true},
+        { label: 'Categorias', path: '/categorias' },
+        { label: 'Favoritos', path: '/favoritos' },
+        { label: 'Sobre', path: '/sobre' }
+    ]
+
     return (
         <header>
             <nav>
                 <ul>
-                    <li><NavLink to={"/"}>Página inicial</NavLink></li>
-                    <li><NavLink to={"/categorias"}>Categorias</NavLink></li>
-                    <li><NavLink to={"/favoritos"}>Favoritos</NavLink></li>
-                    <li><NavLink to={"/sobre"}>Sobre</NavLink></li>
+                    {/* Mapeia os itens do menu para links de navegação e testa a rota ativa */}
+                    {menuItems.map(({ label, path, end }) => (
+                        <li key={path}>
+                            <NavLink
+                                to={path}
+                                end={end}
+                                className={({ isActive }) => (isActive ? 'active' : '')}
+                            >
+                                {label}
+                            </NavLink>
+                        </li>
+                    ))}
                 </ul>
             </nav>
         </header>
