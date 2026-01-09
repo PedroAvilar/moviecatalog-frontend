770cd6592b746ac64a0cbdd0c829cfe6fdc43f7b
Pedro Wilson Araújo Avilar <pedrowa.avilar@gmail.com>
2026-01-09 17:34:51 -0300

    refactor: rename header file to Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './styles/App.css'
-import Header from './components/header.jsx';
+import Header from './components/Header.jsx';
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
