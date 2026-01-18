c97802c8280009bda796dce97b1fe40d961be036
Pedro Wilson Araújo Avilar <pedrowa.avilar@gmail.com>
2026-01-18 18:54:12 -0300

    feat: include for top-rated movies on  home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,16 +2,25 @@
 import { useEffect, useState } from 'react';
 import Banner from '../components/banner/Banner';
 import MovieSection from '../components/movieSection/MovieSection';
-import { getPopularMovies } from '../services/tmdbService';
+import { getPopularMovies, getTopRatedMovies } from '../services/tmdbService';
 
 function Home() {
+    // Estados para armazenar as listas de filmes (inicia arrays vazios)
     const [popular, setPopular] = useState([]);
+    const [topRated, setTopRated] = useState([]);
 
+    // Hook para executar a busca de dados assim que o componente iniciar
     useEffect(() => {
+        // Busca e atualiza o estado de "Populares"
         getPopularMovies().then(data => {
             setPopular(data.results);
         });
-    }, []);
+
+        // Busca e atualiza o estado de "Melhores availados"
+        getTopRatedMovies().then(data => {
+            setTopRated(data.results);
+        })
+    }, []); // Indica que o efeito roda apenas uma vez
 
     return (
         <div>
@@ -20,6 +29,11 @@ function Home() {
             <MovieSection 
                 title={'Populares'}
                 movies={popular}
+            />
+
+            <MovieSection 
+                title={'Melhores avaliados'}
+                movies={topRated}
             />
         </div>
     )
