b259a3ee50eb5b5d5803ac489b50944132716e1b
Pedro Wilson Araújo Avilar <pedrowa.avilar@gmail.com>
2026-01-19 13:16:40 -0300

    fix: basename for correct navigation

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import './styles/index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 
-const basename = '/moviecatalog-frontend';
+const basename = '/moviecatalog-frontend/';
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter basename={basename}>
