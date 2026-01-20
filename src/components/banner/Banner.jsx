d33c7baf5584cc3dd8a52503c223de08c9ac992f
Pedro Wilson Araújo Avilar <pedrowa.avilar@gmail.com>
2026-01-20 18:42:49 -0300

    style: animations for the banner

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -6,12 +6,18 @@ import { slugify } from '../../utils/slugify';
 function Banner({ movies }) {
     const navigate = useNavigate(); // Hook para navegação programática
     const [currentIndex, setCurrentIndex] = useState(0); // Estado que controla qual filme está sendo exibido
+    const [fade, setFade] = useState(true); // Estado que controla o fade
 
     useEffect(() => {
         if (!movies || movies.length === 0) return; // Caso sem filmes, sem temporizador
 
         const interval = setInterval(() => {
-            setCurrentIndex(prev => (prev + 1) % movies.length);
+            setFade(false);
+
+            setTimeout(() => {
+                setCurrentIndex(prev => (prev + 1) % movies.length);
+                setFade(true);
+            }, 300);
         }, 8000); // Troca o índice a cada 8 segundos
 
         return () => clearInterval(interval); // Função de limpeza
@@ -23,11 +29,13 @@ function Banner({ movies }) {
     const titleSlug = slugify(movie.title); // Gera slug do título do filme
 
     return (
-        <section 
-            className='banner'
-            // Define a imagem de fundo dinamicamente usando o backdrop (imagem horizontal) da API
-            style={{backgroundImage: `url(https://image.tmdb.org/t/p/w1280${movie.backdrop_path})`}}
-        >
+        <section className='banner' key={movie.id}>
+            <div 
+                className={`banner-image ${fade? 'fade-in' : 'fade-out'}`}
+                // Define a imagem de fundo dinamicamente usando o backdrop (imagem horizontal) da API
+                style={{backgroundImage: `url(https://image.tmdb.org/t/p/w1280${movie.backdrop_path})`}}
+            />
+        
             <div 
                 className='banner-overlay'
                 onClick={() => navigate(`/filme/${movie.id}/${titleSlug}`)} // Navega para detalhes do filme
@@ -53,7 +61,12 @@ function Banner({ movies }) {
                             className={`dot ${index === currentIndex ? 'active' : ''}`}
                             onClick={(e) => {
                                 e.stopPropagation();
-                                setCurrentIndex(index);
+                                setFade(false);
+
+                                setTimeout(() => {
+                                    setCurrentIndex(index);
+                                    setFade(true);
+                                }, 300);
                             }}
                         />
                     ))}
