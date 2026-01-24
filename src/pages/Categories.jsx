100a229972c2cb6b9d71307fd61d4ae7f209ded6
Pedro Wilson Araújo Avilar <pedrowa.avilar@gmail.com>
2026-01-24 16:17:12 -0300

    feat: implement infinite scrolling for movie categories with IntersectionObserver

--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -1,8 +1,68 @@
 // Página de categorias do catálogo de filmes
+import { useEffect, useRef, useState } from "react";
+import { useParams } from "react-router-dom";
+import { getMoviesByGenre } from '../services/tmdbService'
+import MovieSection from "../components/movieSection/MovieSection";
+
 function Categories() {
+    const { genreId, genreName } = useParams(); // Obtém o ID e nome da categoria da URL
+
+    // Estados para dados, controle de página e carregamento
+    const [movies, setMovies] = useState([]);
+    const [page, setPage] = useState(1);
+    const [loading, setLoading] = useState(false);
+    const [hasMore, setHasMore] = useState(true);
+
+    const observerRef = useRef(null); // Referência para o elemento no final da página
+
+    // Efeito para resetar quando mudar de categoria
+    useEffect(() => {
+        setMovies([]);
+        setPage(1);
+        setHasMore(true);
+    }, [genreId]);
+
+    // Efeito para buscar filmes sempre que a página ou categoria mudar
+    useEffect(() => {
+        async function fetchMovies() {
+            if (loading || !hasMore) return;
+
+            setLoading(true);
+            const data = await getMoviesByGenre(genreId, page);
+
+            if (data.results.length === 0) {
+                setHasMore(false);
+            } else {
+                setMovies(prev => [...prev, ...data.results]); // Concatena os filmes
+            }
+            setLoading(false);
+        }
+        fetchMovies();
+    }, [page, genreId]);
+
+    // Efeito para configurar o IntersectionObserver
+    useEffect(() => {
+        const observer = new IntersectionObserver(entries => {
+            if (entries[0].isIntersecting && hasMore && !loading) {
+                setPage(prev => prev + 1);
+            }
+        });
+        if (observerRef.current) observer.observe(observerRef.current);
+
+        return () => observer.disconnect();
+    }, [hasMore, loading]);
+
     return (
         <div>
-            <h1>Categorias</h1>
+            <MovieSection 
+                title={genreName}
+                movies={movies}
+            />
+
+            {loading && <p>Carregando mais filmes...</p>}
+
+            {/* Quando visível, o observer detecta e carrega mais */}
+            <div ref={observerRef} style={{ height: '1px' }}/>
         </div>
     )
 }
