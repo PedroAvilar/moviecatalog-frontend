18f9608012a05c3414e2d561b50280b89c5fcd35
Pedro Wilson Araújo Avilar <pedrowa.avilar@gmail.com>
2026-01-23 22:59:57 -0300

    feat: add empty state design for favorites page with navigation to home page

--- src/pages/favorites/Favorites.jsx
+++ src/pages/favorites/Favorites.jsx
@@ -3,9 +3,11 @@ import { useEffect, useState } from 'react';
 import { getFavorites } from '../../services/favoritesService';
 import MovieSection from '../../components/movieSection/MovieSection';
 import './favorites.css'
+import { useNavigate } from 'react-router-dom';
 
 function Favorites() {
     const [favorites, setFavorites] = useState([]);
+    const navigate = useNavigate();
 
     useEffect(() => {
         setFavorites(getFavorites());
@@ -15,7 +17,14 @@ function Favorites() {
     if (favorites.length == 0) {
         return (
             <div className='no-favorites'>
-                <p>Você não adicionou filmes aos favoritos.</p>
+                <div className='no-favorites-content'>
+                    <span className='no-favorites-icon'>💔</span>
+                    <h2>Nenhum filme adicionado</h2>
+                    <p>Adicione filmes aos favoritos para aparecerem aqui.</p>
+                    <button onClick={() => navigate('/')}>
+                        Explorar filmes
+                    </button>
+                </div>
             </div>
         )
     } else {
