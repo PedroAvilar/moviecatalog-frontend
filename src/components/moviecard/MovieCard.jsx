51379fae09df7613920951faef401a70f9b01dd2
Pedro Wilson Araújo Avilar <pedrowa.avilar@gmail.com>
2026-01-29 20:42:28 -0300

    style: enhance MovieCard with fade-in effect for poster loading

--- src/components/moviecard/MovieCard.jsx
+++ src/components/moviecard/MovieCard.jsx
@@ -1,9 +1,11 @@
 import { useNavigate } from 'react-router-dom';
 import './movieCard.css';
 import { slugify } from '../../utils/slugify';
+import { useState } from 'react';
 
 function MovieCard ({ id, title, poster, rating }) {
     const navigate = useNavigate(); /* Hook para navegação programática */
+    const [isLoaded, setIsLoaded] = useState(false); /* Estado para fade-in */
     
     const titleSlug = slugify(title); /* Gera slug do título do filme */
 
@@ -12,11 +14,15 @@ function MovieCard ({ id, title, poster, rating }) {
             className='movie-card'
             onClick={() => navigate(`/filme/${id}/${titleSlug}`)} /* Navega para detalhes do filme */
         >
-            <img 
-                src={poster} 
-                alt={`Poster do filme ${title}`}
-                className='movie-poster'
-            />
+            <div className={`poster-container ${!isLoaded ? 'skeleton-poster' : ''}`}>
+                <img 
+                    src={poster} 
+                    alt={`Poster do filme ${title}`}
+                    className={`movie-poster ${isLoaded ? 'loaded' : ''}`}
+                    onLoad={() => setIsLoaded(true)}
+                    loading='lazy'
+                />
+            </div>
             <h3 className='movie-title'>{title}</h3>
             {rating && <p className='movie-rating'>⭐ {rating}</p>}
         </article>
