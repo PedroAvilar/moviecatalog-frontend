cda6226e310655448a5bbb4c33da8199e1e21f22
Pedro Wilson Araújo Avilar <pedrowa.avilar@gmail.com>
2026-02-07 20:19:03 -0300

    refactor: apply transition system to movie card

--- src/components/moviecard/MovieCard.jsx
+++ src/components/moviecard/MovieCard.jsx
@@ -1,12 +1,13 @@
 import { useNavigate } from 'react-router-dom';
 import './movieCard.css';
 import '../../styles/skeleton.css';
+import '../../styles/transitions.css';
 import { slugify } from '../../utils/slugify';
 import { useState } from 'react';
 
 function MovieCard ({ id, title, poster, rating }) {
     const navigate = useNavigate(); /* Hook para navegação programática */
-    const [isLoaded, setIsLoaded] = useState(false); /* Estado para fade-in */
+    const [isLoaded, setIsLoaded] = useState(false); /* Estado para fade-in em transitions */
     
     const titleSlug = slugify(title); /* Gera slug do título do filme */
 
@@ -15,11 +16,12 @@ function MovieCard ({ id, title, poster, rating }) {
             className='movie-card'
             onClick={() => navigate(`/filme/${id}/${titleSlug}`)} /* Navega para detalhes do filme */
         >
-            <div className={`poster-container ${!isLoaded ? 'skeleton-base' : ''}`}>
+            {/* Wrapper com skeleton */}
+            <div className={`movie-card-poster-wrapper ${!isLoaded ? 'skeleton-base' : ''}`}> 
                 <img 
                     src={poster} 
                     alt={`Poster do filme ${title}`}
-                    className={`movie-poster ${isLoaded ? 'loaded' : ''}`}
+                    className={`movie-card-poster fade ${isLoaded ? 'show' : ''}`}
                     onLoad={() => setIsLoaded(true)}
                     loading='lazy'
                 />
