46d68290b235b0e00f6997fbc42f4ef33ebfaa56
Pedro Wilson Araújo Avilar <pedrowa.avilar@gmail.com>
2026-02-10 16:49:47 -0300

    refactor: apply transition system to cast list

--- src/components/castList/CastList.jsx
+++ src/components/castList/CastList.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import { getProfileUrl } from "../../utils/getProfileUrl";
 import './castList.css';
+import '../../styles/skeleton.css';
 
+// Componente principal para lista de atores
 function CastList({ cast }) {
     if (!cast || cast.length === 0) return null;
 
@@ -9,19 +12,31 @@ function CastList({ cast }) {
             <h2>Elenco principal</h2>
             <div className="cast-scroll">
                 {cast.slice(0, 15).map(actor => (
-                    <div key={actor.id} className="cast-card">
-                        <img
-                            src={getProfileUrl(actor.profile_path)}
-                            alt={actor.name}
-                            className="cast-image"
-                            loading="lazy"
-                        />
-                        <div className="cast-info">
-                            <p className="cast-name">{actor.name}</p>
-                            <p className="cast-character">{actor.character}</p>
-                        </div>
-                    </div>
+                    <CastCard key={actor.id} actor={actor} />
                 ))}
+            </div>
+        </div>
+    );
+}
+
+// Componente para exibir cada ator
+function CastCard({ actor}) {
+    const [loaded, setLoaded] = useState(false); // Estado para transitions
+
+    return (
+        <div className="cast-card">
+            <div className={`cast-image-wrapper ${!loaded ? 'skeleton-base' : ''}`}>
+                <img 
+                    src={getProfileUrl(actor.profile_path)}
+                    alt={actor.name} 
+                    className={`cast-image fade fade-fast ${loaded ? 'show' : ''}`}
+                    onLoad={() => setLoaded(true)}
+                    loading="lazy"
+                />
+            </div>
+            <div className="cast-info">
+                <p className="cast-name">{actor.name}</p>
+                <p className="cast-character">{actor.character}</p>
             </div>
         </div>
     );
