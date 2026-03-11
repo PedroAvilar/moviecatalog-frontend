823c1a3701127a97f02ebacbe639420f1a0584dc
Pedro Wilson Araújo Avilar <pedrowa.avilar@gmail.com>
2026-03-11 20:59:21 -0300

    refactor: remove unnecessary comments

--- src/components/castList/CastListSkeleton.jsx
+++ src/components/castList/CastListSkeleton.jsx
@@ -4,19 +4,16 @@ import '../../styles/skeleton.css'
 function CastListSkeleton() {
     return (
         <div className='skeleton-cast-container'>
-            {/* Título h2 */}
+            
             <div className='skeleton-cast-title skeleton-base'/>
 
             <div className='skeleton-cast-scroll'>
 
-                {/* Cards */}
                 {Array.from({ length: 10 }).map((_,index) => (
                     <div key={index} className='skeleton-cast-card'>
 
-                        {/* Imagem */}
                         <div className='skeleton-cast-image skeleton-base'/>
 
-                        {/* Info com name e character */}
                         <div className='skeleton-cast-info'>
                             <div className='skeleton-cast-name skeleton-base'/>
                             <div className='skeleton-cast-character skeleton-base'/>
