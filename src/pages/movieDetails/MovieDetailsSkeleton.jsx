5be6bb10eb1cd21cd46fb25b627c9f02fe53c86c
Pedro Wilson Araújo Avilar <pedrowa.avilar@gmail.com>
2026-03-12 00:13:03 -0300

    refactor: adjustments to comments

--- src/pages/movieDetails/MovieDetailsSkeleton.jsx
+++ src/pages/movieDetails/MovieDetailsSkeleton.jsx
@@ -6,29 +6,24 @@ function MovieDetailsSkeleton() {
         <article className='skeleton-details-wrapper'>
             <section className='skeleton-details'>
 
-                {/* Poster */}
                 <div className='skeleton-details-poster skeleton-base'/>
 
                 <div className='skeleton-details-info'>
-                    {/* Título h1 */}
+
                     <div className='skeleton-details-title skeleton-base'/>
 
-                    {/* Rating | Runtime | Genres */}
                     <div className='skeleton-details-group'>
                         <div className='skeleton-details-line small skeleton-base'/>
                         <div className='skeleton-details-line small skeleton-base'/>
                         <div className='skeleton-details-line medium skeleton-base'/>
                     </div>
 
-                    {/* Subtítulo h2 */}
                     <div className='skeleton-details-subtitle skeleton-base'/>
 
-                    {/* Descrição em 3 linhas */}
                     <div className='skeleton-details-line full skeleton-base'/>
                     <div className='skeleton-details-line full skeleton-base'/>
                     <div className='skeleton-details-line full skeleton-base'/>
 
-                    {/* Favorito */}
                     <div className='skeleton-details-favorite-btn'>
                         <div className='skeleton-details-subtitle skeleton-base'/>
                         <div className='skeleton-details-btn skeleton-base'/>
