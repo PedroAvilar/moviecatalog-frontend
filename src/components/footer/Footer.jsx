d427c0ee625feeb96ad5f2260385dfb906f881e3
Pedro Wilson Araújo Avilar <pedrowa.avilar@gmail.com>
2026-03-17 19:00:54 -0300

    refactor: change the year and class logic to css

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,11 @@
 import './footer.css';
 
 function Footer() {
+    const year = new Date().getFullYear();
+
     return (
-        <footer>
-            <p>&copy; MovieCatalog 2026. Todos os direitos reservados.</p>
+        <footer className='footer'>
+            <p>&copy; MovieCatalog {year}. Todos os direitos reservados.</p>
         </footer>
     )
 }
