7f19e2cac75d37d53cbed18316051c528e7ac36d
Pedro Wilson Araújo Avilar <pedrowa.avilar@gmail.com>
2026-03-24 18:33:55 -0300

    feat: add register page with AuthContext

---
+++ src/pages/register/Register.jsx
@@ -0,0 +1,96 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { register } from "../../services/apiService";
+import ErrorMessage from "../../components/errorMessage/ErrorMessage";
+import '../../styles/auth.css';
+import Button from "../../components/button/Button";
+
+function Register() {
+    const [formData, setFormData] = useState({ name: '', email: '', password: ''});
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
+
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.id]: e.target.value });
+    };
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+        try {
+            setLoading(true);
+            setError(null);
+            await register(formData);
+            navigate('/login', { state: { message: 'Cadastro realizado! Faça login.'}})
+        } catch (err) {
+            setError(err.message);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    return (
+        <main className="auth-container">
+            <form className="auth-form" onSubmit={handleSubmit}>
+                <h2>Criar conta</h2>
+
+                <div className="auth-input-wrapper">
+
+                    <div className="auth-input-group">
+                        <label htmlFor="name">Nome</label>
+                        <input 
+                            type="text"
+                            id="name"
+                            onChange={handleChange}
+                            required
+                            placeholder="Pedro"
+                        />
+                    </div>
+
+                    <div className="auth-input-group">
+                        <label htmlFor="email">E-mail</label>
+                        <input 
+                            type="email"
+                            id="email"
+                            onChange={handleChange}
+                            required
+                            placeholder="pedro@exemplo.com"
+                        />
+                    </div>
+
+                    <div className="auth-input-group">
+                        <label htmlFor="password">Senha</label>
+                        <input 
+                            type="password"
+                            id="password"
+                            onChange={handleChange}
+                            required
+                            placeholder="••••••••"
+                        />
+                    </div>
+
+                    <Button
+                        type="submit"
+                        disabled={loading}
+                        variant="primary"
+                    >
+                        Cadastrar
+                    </Button>
+                </div>
+
+                {error && (
+                    <ErrorMessage
+                        message={error}
+                        variant="compact"
+                    />
+                )}
+
+                <p className="auth-footer">
+                    Já possui conta? <Link to='/login'>Fazer login</Link>
+                </p>
+            </form>
+        </main>
+    )
+}
+
+export default Register;
