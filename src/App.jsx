5cbde7d1ab1a32818756c4bb80089683527f00d3
Pedro Wilson Araújo Avilar <pedrowa.avilar@gmail.com>
2026-03-26 20:26:33 -0300

    feat: add routes for new pages and ProtectedRoute

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,9 @@ import About from './pages/about/About.jsx';
 import MovieDetails from './pages/movieDetails/MovieDetails.jsx';
 import Login from './pages/login/Login.jsx';
 import Register from './pages/register/Register.jsx';
+import { ProtectedRoute } from './components/protectedRoute/ProtectedRoute.jsx';
+import MyReviews from './pages/myReviews/MyReviews.jsx';
+import Profile from './pages/profile/Profile.jsx';
 
 function App() {
 
@@ -20,11 +23,13 @@ function App() {
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/categorias/:genreId/:genreName' element={<Categories/>}/>
-          <Route path='/favoritos' element={<Favorites/>}/>
+          <Route path='/favoritos' element={<ProtectedRoute> <Favorites/> </ProtectedRoute>} />
           <Route path='/sobre' element={<About/>}/>
           <Route path='/filme/:id/:title?' element={<MovieDetails />} />
           <Route path='/login' element={<Login />} />
           <Route path='/cadastro' element={<Register />} />
+          <Route path='/minhas-avaliacoes' element={<ProtectedRoute> <MyReviews /> </ProtectedRoute>} />
+          <Route path='/perfil' element={<ProtectedRoute> <Profile /> </ProtectedRoute>} />
         </Routes>
       </main>
 
