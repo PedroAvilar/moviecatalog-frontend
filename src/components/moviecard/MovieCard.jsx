e0964255c43e85e29b1efbf023d80a81ed8e6bc9
Pedro Wilson Araújo Avilar <pedrowa.avilar@gmail.com>
2026-03-28 19:03:42 -0300

    refactor: MoviePoster added to MovieCard and unused styles removed

--- src/components/moviecard/MovieCard.jsx
+++ src/components/moviecard/MovieCard.jsx
@@ -1,15 +1,11 @@
 import { useNavigate } from 'react-router-dom';
-import './movieCard.css';
-import '../../styles/skeleton.css';
-import '../../styles/transitions.css';
 import { slugify } from '../../utils/slugify';
-import { useState } from 'react';
 import FavoriteButton from '../favoriteButton/FavoriteButton';
+import MoviePoster from '../moviePoster/MoviePoster';
+import './movieCard.css';
 
-function MovieCard ({ id, title, poster, rating }) {
+function MovieCard ({ id, title, poster_path, rating }) {
     const navigate = useNavigate();
-    const [isLoaded, setIsLoaded] = useState(false);
-    
     const titleSlug = slugify(title);
 
     return (
@@ -17,22 +13,21 @@ function MovieCard ({ id, title, poster, rating }) {
             className='movie-card'
             onClick={() => navigate(`/filme/${id}/${encodeURIComponent(titleSlug)}`)}
         >
-            <div className={`movie-card-poster-wrapper ${!isLoaded ? 'skeleton-base' : ''}`}> 
-                <img 
-                    src={poster} 
-                    alt={`Poster do filme ${title}`}
-                    className={`movie-card-poster fade ${isLoaded ? 'show' : ''}`}
-                    onLoad={() => setIsLoaded(true)}
-                    loading='lazy'
+            <div className='movie-card-poster-favorite'>
+                <MoviePoster 
+                    path={poster_path}
+                    alt={title}
+                    size='w342'
+                    className='poster-sm'
                 />
                 <FavoriteButton
-                    movie={{ id, title, poster_path: poster }}
+                    movie={{ id, title, poster_path}}
                     size={35}
                     variant='floating'
                 />
             </div>
-            <h3 className='movie-title'>{title}</h3>
-            {rating && <p className='movie-rating'>⭐ {rating}</p>}
+            <h3 className='movie-card-title'>{title}</h3>
+            {rating && <p className='movie-card-rating'>⭐ {rating}</p>}
         </article>
     );
 }
