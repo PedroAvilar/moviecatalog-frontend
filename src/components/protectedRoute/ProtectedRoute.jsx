d43845fea04c81fc42cfce8f8f3c0e186e69a302
Pedro Wilson Araújo Avilar <pedrowa.avilar@gmail.com>
2026-03-29 16:15:31 -0300

    feat: add showToast to ProtectedRoute

--- src/components/protectedRoute/ProtectedRoute.jsx
+++ src/components/protectedRoute/ProtectedRoute.jsx
@@ -1,8 +1,17 @@
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext";
+import { useToast } from "../../context/ToastContext";
+import { useEffect } from "react";
 
 export function ProtectedRoute({ children }) {
     const { signed, loading } = useAuth();
+    const { showToast } = useToast();
+
+    useEffect(() => {
+        if (!loading && !signed) {
+            showToast({ message: 'Faça login para acessar a página'})
+        }
+    }, [loading, signed, showToast]);
 
     if (loading) return null;
 
