ee73bdbaae6c28be4c70708be635cb81aa5d0f9d
Pedro Wilson Araújo Avilar <pedrowa.avilar@gmail.com>
2026-04-20 16:14:52 -0300

    feat: create a query client with global settings and remove FavoritesContext

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { FavoritesProvider } from './context/FavoritesContext.jsx';
 import { AuthProvider } from './context/AuthContext.jsx';
 import { ToastProvider } from './context/ToastContext.jsx';
 import App from './App.jsx'
@@ -8,14 +9,26 @@ import './styles/index.css'
 
 const basename = '/moviecatalog-frontend/';
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+      cacheTime: 1000 * 60 * 30,
+      refetchOnWindowFocus: false,
+      retry: 1
+    },
+  },
+});
+
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename={basename}>
-    <ToastProvider>
-      <AuthProvider>
-        <FavoritesProvider>
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter basename={basename}>
+      <ToastProvider>
+        <AuthProvider>
           <App />
-        </FavoritesProvider>
-      </AuthProvider>
-    </ToastProvider>
-  </BrowserRouter>
+        </AuthProvider>
+      </ToastProvider>
+    </BrowserRouter>
+    <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>
 );
