e7b0d15679b3f3e63e7a7f3dcae97e2a54bb1f8f
Pedro Wilson Araújo Avilar <pedrowa.avilar@gmail.com>
2026-04-21 00:47:04 -0300

    refactor: apply global skeleton files to CastListSkeleton, removing the custom CSS skeleton file

--- src/components/castList/CastListSkeleton.jsx
+++ src/components/castList/CastListSkeleton.jsx
@@ -1,22 +1,22 @@
-import './castListSkeleton.css'
+import './castList.css'
 import '../../styles/skeleton.css'
 
 function CastListSkeleton() {
     return (
-        <div className='skeleton-cast-container'>
-            
-            <div className='skeleton-cast-title skeleton-base'/>
+        <div className='cast-container skeleton-mobile-center' style={{ pointerEvents: 'none', margin: '2rem 0' }}>
 
-            <div className='skeleton-cast-scroll'>
+            <div className='skeleton-text skeleton-h2 skeleton-base' />
 
-                {Array.from({ length: 10 }).map((_,index) => (
-                    <div key={index} className='skeleton-cast-card'>
+            <div className='cast-scroll' style={{ overflowX: 'hidden' }}>
 
-                        <div className='skeleton-cast-image skeleton-base'/>
+                {Array.from({ length: 10 }).map((_, index) => (
+                    <div key={index} className='cast-card'>
 
-                        <div className='skeleton-cast-info'>
-                            <div className='skeleton-cast-name skeleton-base'/>
-                            <div className='skeleton-cast-character skeleton-base'/>
+                        <div className='cast-image-wrapper skeleton-avatar skeleton-base' />
+
+                        <div className='cast-info' style={{ alignItems: 'center' }}>
+                            <div className='skeleton-text skeleton-p skeleton-w-80 skeleton-base' style={{ marginBottom: '0.5rem' }} />
+                            <div className='skeleton-text skeleton-p skeleton-w-60 skeleton-base' />
                         </div>
                     </div>
                 ))}
