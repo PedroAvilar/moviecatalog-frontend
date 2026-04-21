7325523057718d1ee5f8668cc1cbacd3b2976db3
Pedro Wilson Araújo Avilar <pedrowa.avilar@gmail.com>
2026-04-21 00:44:28 -0300

    refactor: apply global skeleton files to MovieDetails, removing the custom skeleton file

--- src/pages/movieDetails/MovieDetailsSkeleton.jsx
+++ src/pages/movieDetails/MovieDetailsSkeleton.jsx
@@ -1,37 +1,41 @@
-import './movieDetailsSkeleton.css';
+import './movieDetails.css';
 import '../../styles/skeleton.css';
 
 function MovieDetailsSkeleton() {
     return (
-        <article className='skeleton-details-wrapper'>
-            <section className='skeleton-details'>
+        <article style={{ pointerEvents: 'none' }}>
+            <section className='movie-details'>
 
-                <div className='skeleton-details-poster skeleton-base'/>
+                <div className='skeleton-poster skeleton-poster-lg skeleton-base' />
 
-                <div className='skeleton-details-info'>
+                <div className='movie-info skeleton-mobile-center' style={{ width: '100%', margin: '0 auto' }}>
 
-                    <div className='skeleton-details-title skeleton-base'/>
-
-                    <div className='skeleton-details-group'>
-                        <div className='skeleton-details-line small skeleton-base'/>
-                        <div className='skeleton-details-line small skeleton-base'/>
-                        <div className='skeleton-details-line medium skeleton-base'/>
+                    <div className='movie-title-icon'>
+                        <div className='skeleton-text skeleton-h1 skeleton-base' />
+                        <div className='skeleton-avatar skeleton-base' style={{ width: '50px', height: '50px' }} />
                     </div>
 
-                    <div className='skeleton-details-subtitle skeleton-base'/>
+                    <div className='movie-meta' style={{ width: '100%' }}>
+                        <div className='skeleton-text skeleton-h3 skeleton-w-10 skeleton-base' />
+                        <div className='skeleton-text skeleton-h3 skeleton-w-10 skeleton-base' />
+                        <div className='skeleton-text skeleton-h3 skeleton-w-40 skeleton-base' />
+                    </div>
 
-                    <div className='skeleton-details-line full skeleton-base'/>
-                    <div className='skeleton-details-line full skeleton-base'/>
-                    <div className='skeleton-details-line full skeleton-base'/>
+                    <div className='skeleton-text skeleton-h2 skeleton-base' />
 
-                    <div className='skeleton-details-subtitle skeleton-base'/>
+                    <div className='skeleton-text skeleton-p skeleton-w-100 skeleton-base' />
+                    <div className='skeleton-text skeleton-p skeleton-w-100 skeleton-base' />
+                    <div className='skeleton-text skeleton-p skeleton-w-100 skeleton-base' />
+                    <div className='skeleton-text skeleton-p skeleton-w-100 skeleton-base' />
 
-                    <div className='skeleton-details-line medium skeleton-base'/>
+                    <div className='skeleton-text skeleton-h2 skeleton-base' />
 
-                    <div className='skeleton-details-subtitle skeleton-base'/>
+                    <div className='skeleton-text skeleton-p skeleton-w-60 skeleton-base'/>
 
-                    <div className='skeleton-details-line full skeleton-base'/>
-                    <div className='skeleton-details-line full skeleton-base'/>
+                    <div className='skeleton-text skeleton-h2 skeleton-base' />
+
+                    <div className='skeleton-text skeleton-p skeleton-w-100 skeleton-base' />
+                    <div className='skeleton-text skeleton-p skeleton-w-100 skeleton-base' />
 
                 </div>
             </section>
