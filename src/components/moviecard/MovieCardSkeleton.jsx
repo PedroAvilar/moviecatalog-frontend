5fcca50618cc287b96106a409e8de4bbac7e6006
Pedro Wilson Araújo Avilar <pedrowa.avilar@gmail.com>
2026-04-21 00:46:13 -0300

    refactor: apply global skeleton files to MovieCardSkeleton, removing the custom CSS skeleton file

--- src/components/moviecard/MovieCardSkeleton.jsx
+++ src/components/moviecard/MovieCardSkeleton.jsx
@@ -1,12 +1,11 @@
-import './movieCardSkeleton.css';
 import '../../styles/skeleton.css';
 
 function MovieCardSkeleton() {
     return (
-        <article className='movie-card skeleton-card'>
-            <div className='skeleton-card-poster skeleton-base'/>
-            <div className='skeleton-card-title skeleton-base' />
-            <div className='skeleton-card-rating skeleton-base'/>
+        <article className='movie-card' style={{ pointerEvents: 'none' }}>
+            <div className='skeleton-poster skeleton-poster-sm skeleton-base' />
+            <div className='skeleton-text skeleton-p skeleton-w-80 skeleton-base' style={{ marginTop: '0.5rem' }} />
+            <div className='skeleton-text skeleton-p skeleton-w-40 skeleton-base' style={{ marginTop: '0.5rem' }} />
         </article>
     );
 }
