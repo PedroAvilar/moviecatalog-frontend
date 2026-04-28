39b775a5a6c29fadffc3f841a514e00f0a1dfe5d
Pedro Wilson Araújo Avilar <pedrowa.avilar@gmail.com>
2026-04-28 20:06:35 -0300

    chore: add vitest, react testing library, jsdom, and msw to your testing environment

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-const repoName = 'moviecatalog-frontend'
+const repoName = 'moviecatalog-frontend';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: `/${repoName}/`
-})
+	plugins: [react()],
+	base: `/${repoName}/`,
+	test: {
+		globals: true,
+		environment: 'jsdom',
+		setupFiles: './src/test/setup.js',
+		css: true,
+	},
+});
